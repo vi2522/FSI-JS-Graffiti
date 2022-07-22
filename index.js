7cab217b39ac4b3de8df3aef460776243fedbf28
Victor Barajas <victorbarajas@Victors-MacBook-Pro.local>
2022-07-21 19:46:39 -0700

    editing the webpage through index.js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Select some elements...
+
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform ='rotate(-180deg)'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = "left"
+}
+
+let footerText = document.querySelector('.footer')
+footerText.style.color = "orange"
+
